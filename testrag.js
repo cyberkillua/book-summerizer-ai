297a831938dec3881dc0bf99cb7c05b45bb78102
cyberkillua <moshood988@gmail.com>
2024-03-26 12:39:02 +0100

    switch to dicuss mode

--- testrag.js
+++ testrag.js
@@ -14,33 +14,41 @@ const llm = new ChatOpenAI({ openAIApiKey: openAIKey, temperature: 0.1 });
 
 export async function askAI(question, senderNumber, phone_number_id) {
   try {
-    console.log("I AM HERE");
     const standaloneQuestionTemplate =
       "Given a question, convert it to a standalone question. question: {question} standalone question:";
     const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
       standaloneQuestionTemplate
     );
 
-    //   "You are a helpful and enthusiastic professional book summarizer bot who can answer a given question about Books based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I cant sumarize that" Don't try to make up an answer. Always speak as if you were chatting to a friend."
-    const answerTemplate = `Act as a professional book summarizer. Your goal is to help the user understand a book without reading it. Concentrate
-on only the most important takeaways and primary points from the book that together will give the user a solid overview
-and understanding of the book and its topic. Include all of the following in your summary: -Main topic or theme of the
-book -Key ideas or arguments presented -All the chapter detailed summaries with the conclusion of the chapter -Key takeaways
-from the book -Comparison to other books on the same subject -Recommendations of other similar books on the same topic
-Utilize both the context provided as well as your own knowledge from the GPT model to write a thorough yet concise
-summary of "{{Book name}}" by {{Author}} based on the instructions provided. If any information in the context contradicts
-your knowledge, prioritize the context.
-OUTPUT: Markdown format with #Headings, ##H2, ###H3, + bullet points, + sub-bullet points
-context: {context}
-question: {question}
-answer: `;
+    const answerTemplate = `Act as a professional book summarizer. Your goal is to 
+    help the user understand a book without reading it. Concentrate on only the most
+    important takeaways and primary points from the book that together will give the
+    user a solid overview and understanding of the book and its topic. Include all of
+    the following in your summary:
+    -Main topic or theme of the book
+    -Key ideas or arguments presented
+    -All the chapter detailed summaries with the conclusion of each chapter  
+    -Key takeaways from the book
+    -Comparison to other books on the same subject
+    -Recommendations of other similar books on the same topic
+    Utilize both the context provided as well as your own knowledge from the GPT model
+    to write a thorough yet concise summary of "{{Book name}}" by {{Author}} based on
+    the instructions provided. If any information in the context contradicts your
+    knowledge, prioritize the context.
+    After providing the summary in Markdown format with #Headings, ##H2, ###H3, + bullet points, + sub-bullet points,
+    be prepared to engage in a thoughtful discussion about the book. Feel free to
+    share your insights, opinions, and analysis related to the themes, ideas, and
+    takeaways from the summary. Additionally, be open to addressing any follow-up
+    questions or comments from the user regarding the book or the summary itself.
+    OUTPUT: Markdown format summary followed by discussion
+    context: {context}
+    question: {question}
+    answer:`;
     const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
 
     const standaloneQuestionChain = standaloneQuestionPrompt
       .pipe(llm)
       .pipe(new StringOutputParser());
-
-    console.log("WANNA RETERIVE");
 
     const retriever = await retriveFromVectorStore();
 
@@ -74,7 +82,3 @@ answer: `;
     console.log(error);
   }
 }
-
-// askAI(
-//   `summarize moshood-alimi-pdf for me based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I cant sumarize that" Don't try to make up an answer. `
-// );
