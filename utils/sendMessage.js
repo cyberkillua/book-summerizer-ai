4c7e0f6b62e8ebe77e3f0742fe79c68379e21ad4
cyberkillua <moshood988@gmail.com>
2024-06-25 16:06:20 +0100

    spliting longer messages into chunks

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,29 +1,37 @@
 import axios from "axios";
+import { splitMessage } from "./splitMessages";
 const token = process.env.CLOUD_API_ACCESS_TOKEN;
 
 export async function send_message(aiAnswer, senderNumber, phone_number_id) {
   try {
     console.log("Sending message to WhatsApp...");
+    let messagesToSend;
     if (aiAnswer.length > 4096) {
-      aiAnswer = aiAnswer.slice(0, 4093) + "...";
+      messagesToSend = splitMessage(aiAnswer, 4000);
+    } else {
+      messagesToSend = [aiAnswer];
     }
+    let index = 0;
+    for (const message of messagesToSend) {
+      index = index + 1;
+      console.log(`sending message number ${index}`);
+      const response = await axios({
+        method: "POST",
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: senderNumber,
+          text: { body: message },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
 
-    const response = await axios({
-      method: "POST",
-      url:
-        "https://graph.facebook.com/v12.0/" +
-        phone_number_id +
-        "/messages?access_token=" +
-        token,
-      data: {
-        messaging_product: "whatsapp",
-        to: senderNumber,
-        text: { body: aiAnswer },
-      },
-      headers: { "Content-Type": "application/json" },
-    });
-
-    return response.data;
+      return response.data;
+    }
   } catch (e) {
     console.log("Error sending message:", e);
   }
---
+++ utils/splitMessages.js
@@ -0,0 +1,7 @@
+export function splitMessage(content, chunkSize) {
+  let chunks = [];
+  for (let i = 0; i < content.length; i += chunkSize) {
+    chunks.push(content.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
